ee36726d674a5bb660cba86aad2eab2bfab74cba
abhishek abhi <abhiabhishek413@gmail.com>
2020-12-29 14:52:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+x = input(' Enter first number\n')
+y = input(' Enter secod number\n')
+
+print("addition, substraction, division and multiplication results given below\n")
+
+addition = int(x)+int(y)
+print("The addition result is \n",addition)
+
+substraction = int(x)-int(y)
+print("The substraction result is \n",substraction)
+
+division = int(x)/int(y)
+print("The division result is \n",division)
+
+multiplication = int(x)*int(y)
+print("The multiplication result is \n",multiplication)
